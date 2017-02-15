07c4e6a4d00c9de882184b479808c2ef0946e975
sophie <nomen@nescio>
2017-02-15 10:52:50 -0700

    include colors for valid moves

--- tafl.py
+++ tafl.py
@@ -5,12 +5,18 @@ import math
 import curses
 from curses import wrapper
 
+# curses.initscr()
+# curses.start_color()
+# curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
 def main(stdscr):
 
     # clear screen
     # stdscr.nodelay(True)
     stdscr.clear()
+
+    # Init color pairs
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_WHITE)
 
     board_size = 9
 
@@ -26,6 +32,8 @@ def main(stdscr):
              0, 0, 0, 0, 1, 0, 0, 0, 0,
              4, 0, 0, 1, 1, 1, 0, 0, 4]
 
+    highlighted_squares = []
+
     def get_moves_in_range(board, start, end, step):
         found_moves = []
         for i in range(start, end, step):
@@ -37,7 +45,7 @@ def main(stdscr):
                 break
         return found_moves
 
-    def get_moves_for_piece(board, coord):
+    def get_moves_for_piece(coord):
         size = int(math.sqrt(len(board)))  # TODO: store this?
         legal_moves = []
         # print(range(coord, math.ceil(coord / size) * size))
@@ -87,13 +95,16 @@ def main(stdscr):
             elif char == 4:
                 return '_'
             elif char == 5:
-                return 'O\u0332'
+                return '\u0332O'
             else:
                 raise Exception('bad character')
 
-        for n in range(board_size):
-            stdscr.addstr(''.join([char_converter(char) for char in board[n * board_size: (n+1) * board_size]]))
-            stdscr.addstr('\n')
+        for i in range(len(board)):
+            attr = []
+            if i in highlighted_squares:
+                attr.append(curses.color_pair(1))
+                stdscr.addstr(11, 0, 'found a thing')
+            stdscr.addstr(math.floor(i / board_size), i % board_size, char_converter(board[i]), *attr)
 
     def move_cursor(direction):
         if direction == 1:
@@ -108,10 +119,19 @@ def main(stdscr):
         else:
             raise Exception('bad direction: ' + direction)
 
+    def yx_to_point(y, x):
+        return y * board_size + x
+
     # Set the board up
 
     print_board()
-    stdscr.move(math.floor(board_size / 2), math.floor(board_size / 2))
+    cursor_loc = (math.floor(board_size / 2), math.floor(board_size / 2))
+
+    # cursor_loc = stdscr.getyx()
+
+    active_square = None
+    active_player = None
+
 
     # . 1 .
     # 4 . 2
@@ -119,16 +139,38 @@ def main(stdscr):
 
     while True:
         direction = None
+        stdscr.move(*cursor_loc)
         c = stdscr.getch()
+        ####
+        # Handle direction keys
+        ####
         if c == curses.KEY_UP:
-            direction = 1
+            stdscr.move(max(0, stdscr.getyx()[0] - 1), stdscr.getyx()[1])
         elif c == curses.KEY_RIGHT:
-            direction = 2
+            stdscr.move(stdscr.getyx()[0], min(board_size - 1, stdscr.getyx()[1] + 1))
         elif c == curses.KEY_DOWN:
-            direction = 3
+            stdscr.move(min(board_size - 1, stdscr.getyx()[0] + 1), stdscr.getyx()[1])
         elif c == curses.KEY_LEFT:
-            direction = 4
-        if direction:
-            move_cursor(direction)
+            stdscr.move(stdscr.getyx()[0], max(0, stdscr.getyx()[1] - 1))
+
+        if c == ord(' '):
+            selected_square = yx_to_point(*stdscr.getyx())
+            if active_square == None: #think this is valid...
+                if board[selected_square] != (0 and 4):
+                    active_square = yx_to_point(*stdscr.getyx())
+                    highlighted_squares = get_moves_for_piece(active_square)
+            elif yx_to_point(*stdscr.getyx()) in highlighted_squares:
+                # complete move
+                stdscr.addstr(10,0,'complete move!')
+                highlighted_squares = []
+                pass
+            else:
+                highlighted_squares = []
+                active_square = None
+
+
+        cursor_loc = stdscr.getyx()
+
+        print_board()
 
 wrapper(main)
