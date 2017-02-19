4ba720de0de633e6694ece910f206683a696f188
sophie <stephen.van.gordon@gmail.com>
2017-02-19 13:29:52 -0700

    rename child nodes as candidate nodes and ecapsulate

--- GameState.py
+++ GameState.py
@@ -34,7 +34,7 @@ class GameState:
                  0, 0, 0, 0, 2, 0, 0, 0, 0,
                  0, 0, 0, 0, 1, 0, 0, 0, 0,
                  4, 0, 0, 1, 1, 1, 0, 0, 4]
-    def __init__(self, active_player=0, board=[], ply=0, previous_moves=[], row_size=9, status="in-play"):
+    def __init__(self, board, active_player=0, ply=0, previous_moves=[], row_size=9, status="in-play", parent=None):
         # Polymorphically set the board
         try:
             board[0]
@@ -46,17 +46,16 @@ class GameState:
             board = list(map(self.piece_constructor, board))
         self.active_player = active_player
         self.board = board
-        self.child_nodes = []
+        self.candidate_nodes = []
+        self.current_state = True if previous_moves == [] else False
+        self.parent = parent
         self.ply = ply
         self.possible_moves = {}
         self.previous_moves = previous_moves
         self.row_size = row_size
         self.status = status
         self.set_possible_moves()
-        if self.ply < self.max_ply and self.status == 'in-play':
-            for i in self.possible_moves:
-                for k in self.possible_moves[i]:
-                    self.generate_child_node(i, k)
+        self.set_candidate_nodes()
 
     @property
     def evaluation(self):
@@ -90,10 +89,10 @@ class GameState:
 
     @property
     def best_move(self):
-        def evaluate_children(child_nodes):
+        def evaluate_children(candidate_nodes):
             best_eval = None
             best_node = None
-            for node in [child_node.best_move for child_node in child_nodes]:
+            for node in [candidate_node.best_move for candidate_node in candidate_nodes]:
                 if best_eval == None:
                     best_eval = node.evaluation
                     best_node = node
@@ -104,10 +103,10 @@ class GameState:
                     best_eval = node.evaluation
                     best_node = node
             return best_node
-        if not self.child_nodes or self.status in ['attacker_wins', 'defender_wins']:
+        if not self.candidate_nodes or self.status in ['attacker_wins', 'defender_wins']:
             return self # has no children or the game is over, so returns self
         else:
-            return evaluate_children(self.child_nodes)
+            return evaluate_children(self.candidate_nodes)
 
     @staticmethod
     def piece_constructor(input_char):
@@ -133,67 +132,80 @@ class GameState:
         else:
             raise Exception('bad char input')
 
-    def generate_child_node(self, move_start, move_end):
-        def check_capture(moving_piece, adjacent_square, bounding_square):
-            # TODO: add support for kings, thrones
-            if adjacent_square == None:
-                return False
-            elif adjacent_square["content"] in [1, 2]:
-                return adjacent_square["owner"] != moving_piece["owner"] \
-                    and bounding_square != None \
-                    and (bounding_square["owner"] == moving_piece["owner"] \
-                        or bounding_square["content"] == 4)
+    def set_candidate_nodes(self):
+        def generate_candidate_node(move_start, move_end):
+            def check_capture(moving_piece, adjacent_square, bounding_square):
+                # TODO: add support for kings, thrones
+                if adjacent_square == None:
+                    return False
+                elif adjacent_square["content"] in [1, 2]:
+                    return adjacent_square["owner"] != moving_piece["owner"] \
+                        and bounding_square != None \
+                        and (bounding_square["owner"] == moving_piece["owner"] \
+                            or bounding_square["content"] == 4)
+            moving_piece = self.board[move_start]
+            new_game_board = copy.deepcopy(self.board)
+            new_status = self.status
+            new_active_player = None
+            new_previous_moves = copy.copy(self.previous_moves)
+            new_previous_moves.append((move_start, move_end))
+            new_game_board[move_start] = self.piece_constructor(4) if moving_piece["content"] == 5 else self.piece_constructor(0)
+            new_game_board[move_end] = moving_piece if moving_piece["content"] != 5 else self.piece_constructor(3)
+            # check to the left
+            adjacent_square = self.board[move_end - 1] if move_end % self.row_size != 0 else None
+            bounding_square = self.board[move_end - 2] if (move_end - 1) % self.row_size != 0 else None
+            if check_capture(moving_piece, adjacent_square, bounding_square):
+                new_game_board[move_end - 1] = self.piece_constructor(0)
+            # check to the right
+            adjacent_square = self.board[move_end + 1] if (move_end + 1) % self.row_size != 0 else None
+            bounding_square = self.board[move_end + 2] if (move_end + 2) % self.row_size != 0 else None
+            if check_capture(moving_piece, adjacent_square, bounding_square):
+                new_game_board[move_end + 1] = self.piece_constructor(0)
+            # check a row above
+            adjacent_square = self.board[move_end - self.row_size] if (move_end - self.row_size) >= 0 else None
+            bounding_square = self.board[move_end - self.row_size * 2] if (move_end - self.row_size * 2) >= 0 else None
+            if check_capture(moving_piece, adjacent_square, bounding_square):
+                new_game_board[move_end - self.row_size] = self.piece_constructor(0)
+            # check a row below
+            adjacent_square = self.board[move_end + self.row_size] if (move_end + self.row_size) < self.row_size * self.row_size else None
+            bounding_square = self.board[move_end + self.row_size * 2] if (move_end + self.row_size * 2) < self.row_size * self.row_size else None
+            if check_capture(moving_piece, adjacent_square, bounding_square):
+                new_game_board[move_end + self.row_size] = self.piece_constructor(0)
+            # Check king position for victory
+            for i in range(self.row_size ** 2):
+                if new_game_board[i]["content"] in [3,5]:
+                    king_position = i
+                    break
+            if king_position in range(0, self.row_size) \
+                or king_position in range(self.row_size * (self.row_size - 1), self.row_size ** 2) \
+                or king_position % self.row_size == 0 \
+                or (king_position + 1) % self.row_size == 0:
+                    new_status = 'defender_wins'
+                    #: in the process of debugging why it thinks 35, 34 kills the king
+            # print('king_position == {0}'.format(king_position))
+            if (king_position - self.row_size < 0 or new_game_board[king_position - self.row_size]["content"] in [1, 4]) \
+                and ((king_position + 1) % self.row_size == 0 or new_game_board[king_position + 1]["content"] in [1, 4]) \
+                and (king_position % self.row_size == 0 or new_game_board[king_position - 1]["content"] in [1, 4]) \
+                and (king_position + self.row_size >= self.row_size ** 2 or new_game_board[king_position + self.row_size]["content"] in [1, 4]):
+                    new_status = 'attacker_wins'
+            new_active_player = (self.active_player + 1) % 2
+            self.candidate_nodes.append(GameState(board=new_game_board, active_player=new_active_player, ply=self.ply + 1, previous_moves=new_previous_moves, row_size=self.row_size, status=new_status, parent=self))
+        if self.ply < self.max_ply and self.status == 'in-play':
+            for i in self.possible_moves:
+                for k in self.possible_moves[i]:
+                    generate_candidate_node(i, k)
 
-        moving_piece = self.board[move_start]
-        new_game_board = copy.deepcopy(self.board)
-        new_game_status = self.status
-        new_active_player = None
-        new_previous_moves = copy.copy(self.previous_moves)
-        new_previous_moves.append((move_start, move_end))
-        new_game_board[move_start] = self.piece_constructor(4) if moving_piece["content"] == 5 else self.piece_constructor(0)
-        new_game_board[move_end] = moving_piece if moving_piece["content"] != 5 else self.piece_constructor(3)
-        # check to the left
-        adjacent_square = self.board[move_end - 1] if move_end % self.row_size != 0 else None
-        bounding_square = self.board[move_end - 2] if (move_end - 1) % self.row_size != 0 else None
-        if check_capture(moving_piece, adjacent_square, bounding_square):
-            new_game_board[move_end - 1] = self.piece_constructor(0)
-        # check to the right
-        adjacent_square = self.board[move_end + 1] if (move_end + 1) % self.row_size != 0 else None
-        bounding_square = self.board[move_end + 2] if (move_end + 2) % self.row_size != 0 else None
-        if check_capture(moving_piece, adjacent_square, bounding_square):
-            new_game_board[move_end + 1] = self.piece_constructor(0)
-        # check a row above
-        adjacent_square = self.board[move_end - self.row_size] if (move_end - self.row_size) >= 0 else None
-        bounding_square = self.board[move_end - self.row_size * 2] if (move_end - self.row_size * 2) >= 0 else None
-        if check_capture(moving_piece, adjacent_square, bounding_square):
-            new_game_board[move_end - self.row_size] = self.piece_constructor(0)
-        # check a row below
-        adjacent_square = self.board[move_end + self.row_size] if (move_end + self.row_size) < self.row_size * self.row_size else None
-        bounding_square = self.board[move_end + self.row_size * 2] if (move_end + self.row_size * 2) < self.row_size * self.row_size else None
-        if check_capture(moving_piece, adjacent_square, bounding_square):
-            new_game_board[move_end + self.row_size] = self.piece_constructor(0)
-
-        # Check king position for victory
-        for i in range(self.row_size ** 2):
-            if new_game_board[i]["content"] in [3,5]:
-                king_position = i
+    def make_move(self, move):
+        """
+        rebinds self to the hcilde node representing the move in question.
+        move is a tuple (move_start, move_end)
+        """
+        for candidate_node in self.candidate_nodes:
+            if candidate_node.previous_moves[-1] == move:
+                print('making move')
+                self = candidate_node
+                # pprint(vars(self))
                 break
-        if king_position in range(0, self.row_size) \
-            or king_position in range(self.row_size * (self.row_size - 1), self.row_size ** 2) \
-            or king_position % self.row_size == 0 \
-            or (king_position + 1) % self.row_size == 0:
-                new_game_status = 'defender_wins'
-                #: in the process of debugging why it thinks 35, 34 kills the king
-        # print('king_position == {0}'.format(king_position))
-        if (king_position - self.row_size < 0 or new_game_board[king_position - self.row_size]["content"] in [1, 4]) \
-            and ((king_position + 1) % self.row_size == 0 or new_game_board[king_position + 1]["content"] in [1, 4]) \
-            and (king_position % self.row_size == 0 or new_game_board[king_position - 1]["content"] in [1, 4]) \
-            and (king_position + self.row_size >= self.row_size ** 2 or new_game_board[king_position + self.row_size]["content"] in [1, 4]):
-                    new_game_status = 'attacker_wins'
-
-        new_active_player = (self.active_player + 1) % 2
-
-        self.child_nodes.append(GameState(new_active_player, new_game_board, self.ply + 1, new_previous_moves, self.row_size, new_game_status))
 
     def set_possible_moves(self):
         def get_moves_in_range(start, end, step):
@@ -238,11 +250,8 @@ class GameState:
 
 game_state = GameState(**init_game_state)
 # game_state.best_move
-# pprint([(child_node.previous_moves, child_node.best_move.evaluation) for child_node in game_state.child_nodes])
-# pprint([(child_node.previous_moves, child_node.evaluation) for child_node in game_state.child_nodes])
 # game_state.best_move
 pprint(vars(game_state.best_move))
-# print(game_state.best_move.previous_moves[-1])
-# print(str([(node.previous_moves[-1], node.evaluation) for node in game_state.child_nodes]))
-# print(str(game_state.best_move))
-# print(str(game_state.child_nodes[0]))
+# game_state.make_move((27, 18))
+# pprint(game_state.previous_moves)
+# pprint(vars(game_state))
